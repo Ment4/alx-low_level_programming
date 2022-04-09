bcaba5cdc946d69cf0d2130d7f6a3377d5f188cf
Minte4 <mihrettolosa30@gmail.com>
2022-04-09 22:49:23 +0000

    my commit on task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Return: 0 if exited properly, non-zero otherwish
  */
 int main(void)
-{ 
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
