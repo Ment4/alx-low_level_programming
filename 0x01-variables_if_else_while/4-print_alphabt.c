2c70e24abc2553d4959a551db9ac4c40140a95ac
Minte4 <mihrettolosa30@gmail.com>
2022-04-10 20:07:24 +0000

    My print alphabt task 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/* File: 4-print_alphabt
+ * Auth: Minte
+*/
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+else if (alphabet == 'e')
+continue;
+putchar(alphabet);
+}
+putchar('\n');
+
+returen (0);
+}
