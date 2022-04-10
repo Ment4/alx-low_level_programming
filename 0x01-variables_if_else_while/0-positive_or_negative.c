ed838ab6d377c33bda78d09509eb1a3db2a59b3f
Minte4 <mihrettolosa30@gmail.com>
2022-04-10 13:01:56 +0000

    My commit on positive and negative c programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,8 @@
 
 /**
  * main - prints a random number and states whether
- *it is positive, negative, or zero.
- *
- *Return: Always 0. 
+ * it is positive, negative, or zero.
+ * Return: Always 0. 
 */
 int main(void)
 {
