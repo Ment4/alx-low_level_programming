d01167109fdbff92773625c2d468090cfe319bb1
Minte4 <mihrettolosa30@gmail.com>
2022-04-11 09:43:24 +0000

    digit_numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,13 @@
  * Return: Always 0.
  */
 int main(void)
+{
+	int i;
+	for (i = 0 ; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
 
-{
-putchar (int num);
-for (num = 0; num < 10; num++)
-{
-putchar(num);
-}
-putchar ("\n");
-return (0);
+	return (0);
 }
