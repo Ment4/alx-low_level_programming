4482aa115bdba853b53d4bff7427b8b810585c12
Minte4 <mihrettolosa30@gmail.com>
2022-04-11 08:15:56 +0000

    My commit on print alphabet c program

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,8 +3,9 @@
 * Auth: Minte
 */
 #include <stdio.h>
+
 /**
- * main - Prints the alphabet in lowercase, except for q and e.
+ * main - prints the alphabet in lowercase, except for q and e.
  *
  * Return: Always 0 (Success/correct)
 */
