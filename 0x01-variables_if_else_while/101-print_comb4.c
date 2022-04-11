2db8a4fcf7e73ee7689ee456de5db5ab04b05570
Minte4 <mihrettolosa30@gmail.com>
2022-04-11 20:54:45 +0000

    combinations of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/*
+ * File: 101-print_comb4.c
+ * Auth: Minte
+ */
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of there digits.
+ * Return: 0
+ */
+int main(void)
+{
+int n, m, l;
+for (n = 48; n < 58; n++)
+{
+for (m = 49; m < 58; m++)
+{
+for (l = 50; l < 58; l++)
+{
+if (l > m && m > n)
+{
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
