412bcd337efbab034db04e6e2acb40b2fadd9924
Ment4 <mihrettolosa30@gmail.com>
2022-04-13 20:44:46 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *
- * print_alphabet - prints alphabet in lowercase
- *
+ * print_alphabet - entry point
+ * Description: prints alphabet in lowercase
+ * Return: void
  */
 void print_alphabet(void)
 {
