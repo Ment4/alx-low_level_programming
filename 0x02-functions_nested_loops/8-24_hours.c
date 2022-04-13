38a8634e348da5f156ab690b1a87b4cdb291cd95
Ment4 <mihrettolosa30@gmail.com>
2022-04-13 20:59:14 +0000

    Jack_bauer-time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * jack_bever - prints every minute of the day 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer,starting from 00:00 to 23:59
  * @void - no argument
  */
-void jack_bever(void)
+void jack_bauer(void)
 {
 int i, j;
 for (i = 0; i < 24; i++)
