9a4351bda508673cbc5121ffbb82d2a10f81c052
Ment4 <mihrettolosa30@gmail.com>
2022-04-13 12:50:08 +0000

    alphabet_x10

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - main block
+ * Describtion: Print_alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char k;
+for (k = 'a'; k <= 'z'; k++)
+_putchar(k);
+_putchar('\n');
+}
